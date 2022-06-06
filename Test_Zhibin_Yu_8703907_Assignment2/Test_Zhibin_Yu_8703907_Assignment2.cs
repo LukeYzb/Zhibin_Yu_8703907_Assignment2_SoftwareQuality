5ea60facd88ae00d463dc03c262a2f047bcf23ef
于志斌 <1355958493@qq.com>
2022-06-06 14:45:31 -0400

    contain the completed Rectangle class

---
+++ Test_Zhibin_Yu_8703907_Assignment2/Test_Zhibin_Yu_8703907_Assignment2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Zhibin_Yu_8703907_Assignment2
+{
+    public class Test_Zhibin_Yu_8703907_Assignment2
+    {
+
+    }
+}
---
+++ Zhibin_Yu_8703907_Assignment2/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zhibin_Yu_8703907_Assignment2
+{
+    internal class Rectangle
+    {
+        private int length;
+        private int width;
+
+        public int GetLength() { return length; }
+        public int SetLength(int length) { this.length = length; return length; }
+        public int GetWidth() { return width; }
+        public int SetWidth(int width) { this.width = width; return width; }
+        public int GetPerimeter() { return (length+width)*2; }
+        public int GetArea() { return length*width; }
+
+        public Rectangle()
+        {
+            this.length = 1;
+            this.width = 1;
+        }
+        public Rectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+    }
+}
